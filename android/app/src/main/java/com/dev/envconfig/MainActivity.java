709d0c088c4f5f59b5e13ef77db24d45354547b2
hmtri <hmtri.it@gmail.com>
2021-06-10 23:34:03 +0700

    init project env config v1 final

---
+++ android/app/src/main/java/com/dev/envconfig/MainActivity.java
@@ -0,0 +1,7 @@
+package com.dev.envconfig;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo Config ENV',
+
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(FlutterConfig.get("APP_NAME")),
+        ),
+      ),
+    );
+  }
+
+}
